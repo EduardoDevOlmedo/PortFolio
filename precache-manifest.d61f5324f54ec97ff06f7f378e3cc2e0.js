5857ec2085b8e2c2e044dc137492929024c7f8bb
Eduardo Olmedo <eduardolmedi00@gmail.com>
2021-11-02 17:51:36 -0600

    Updates

---
+++ precache-manifest.d61f5324f54ec97ff06f7f378e3cc2e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d45c21e18df2335929b",
+    "url": "/EduardoDevOlmedo/PortFolio/static/css/main.d3ebc914.chunk.css"
+  },
+  {
+    "revision": "0d45c21e18df2335929b",
+    "url": "/EduardoDevOlmedo/PortFolio/static/js/main.0d45c21e.chunk.js"
+  },
+  {
+    "revision": "bc5c4fb3ec2574a97922",
+    "url": "/EduardoDevOlmedo/PortFolio/static/js/1.bc5c4fb3.chunk.js"
+  },
+  {
+    "revision": "370cd385363dddb04e5f",
+    "url": "/EduardoDevOlmedo/PortFolio/static/js/runtime~main.370cd385.js"
+  },
+  {
+    "revision": "c95daf0552672db374dcf7401a687672",
+    "url": "/EduardoDevOlmedo/PortFolio/index.html"
+  }
+];

607bce3e27bccf379d27cd74bee466d84c723d86
EDev <eduardolmedi00@gmail.com>
2021-12-14 13:31:44 -0600

    done some fixes

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Title";
+
+function Contacts(){
+    return(
+        <div id="contact">
+            <Title title="contact me" />
+        </div>
+    )
+}
+
+export default Contacts
